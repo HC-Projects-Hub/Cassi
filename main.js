116ac57a8fd4cdf7ee2a1424107784bd48bff9c9
Christian J. Dennis <aottgpvp@gmail.com>
2025-04-28 01:38:16 -0300

    Initial front end setup commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// main.js (in the frontend folder)
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Points to the Next.js development server
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
